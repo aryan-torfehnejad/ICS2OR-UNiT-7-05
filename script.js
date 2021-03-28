de4840bc612ac070a92cb74933137c57b6bbb3c6
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-28 21:58:53 +0000

    fixed

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-let age=0
-let day=0
+let age = 0
+let day = 0
 
-document.getElementById('button').addEventListener('click',click)
-function click(){
-  age=document.getElementById('text2').value
-  age= parseInt(age)
-  day= document.getElementById('text').value 
-  if (age<=0) {
-  alert ('ERROR AGE MUST BE HIGHER THEN 0')
-} else if(age < 18 && day !='Sunday' && day !='Saturday') {
-  document.getElementById('answer').innerHTML='Time for School :('
-} else if (age>= 18 && day !='Sunday' || day !='Saturday'){ document.getElementById('answer').innerHTML='Time for Work :('
-} else {
-  document.getElementById('answer').innerHTML='Time to relax for the weekend :)'
+document.getElementById('button').addEventListener('click', click)
+function click (){
+  age = document.getElementById('text2').value
+  age = parseInt(age)
+  day = document.getElementById('text').value 
+  if (age < = 0) {
+    alert('ERROR AGE MUST BE HIGHER THEN 0')
+  } else if (age < 18 && day ! = ='Sunday' && day ! = ='Saturday') {
+    document.getElementById('answer').innerHTML = 'Time for School :('
+  } else if 
+  ( age > = 18 & day ! = ='Sunday' | day ! = ='Saturday'){ document.getElementById('answer').innerHTML = 'Time for Work :('
+  } else {
+  document.getElementById('answer').innerHTML = 'Time to relax for the weekend :)'
 }
 }
